e1e85a9626257186aa5504ced5d64fdabe744c27
Ayib201 <ayibt2612@gmail.com>
2025-01-20 06:17:01 +0000

    changement terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,7 +21,7 @@ resource "aws_instance" "dev_instance" {
     sudo apt-get install -y docker.io
     sudo systemctl start docker
     sudo systemctl enable docker
-    sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo apt-get install docker-compose -y
     sudo chmod +x /usr/local/bin/docker-compose
     sudo apt-get install -y maven
     # Cloner le repo contenant les fichiers docker-compose
