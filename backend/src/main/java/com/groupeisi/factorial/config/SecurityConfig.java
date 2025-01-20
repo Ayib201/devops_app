04f6408f3fc68b2d14af5250f4cb4f202e352b0c
Souleymane Fall <161875016+JulesBestDev176@users.noreply.github.com>
2025-01-20 18:59:11 +0000

    Update SecurityConfig.java

--- backend/src/main/java/com/groupeisi/factorial/config/SecurityConfig.java
+++ backend/src/main/java/com/groupeisi/factorial/config/SecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class SecurityConfig {
