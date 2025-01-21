7a7cc7bae88bd21fae5ea0e4f16e1d0c71f8fc93
Souleymane Fall <161875016+JulesBestDev176@users.noreply.github.com>
2025-01-21 03:32:58 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,9 +5,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "dev_instance" {
-  ami           = "ami-04b4f1a9cf54c11d0"  # Vérifiez l'AMI pour la région
+  ami           = "ami-06e02ae7bdac6b938"  # Vérifiez l'AMI pour la région
   instance_type = "t2.micro"
-  key_name      = ""                # Assurez-vous que cette clé SSH existe
+  key_name      = "terraformkey"                # Assurez-vous que cette clé SSH existe
   security_groups = ["mon site"]          # Groupe de sécurité avec règles appropriées
 
   tags = {
