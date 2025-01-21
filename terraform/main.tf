da1c67b60d1d82ee9b0f3f9d48b0346c2ceb19f5
Ayib201 <ayibt2612@gmail.com>
2025-01-21 16:21:17 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "dev_instance" {
   ami           = "ami-04b4f1a9cf54c11d0"  # Vérifiez l'AMI pour la région
   instance_type = "t2.micro"
-               # Assurez-vous que cette clé SSH existe
+  key_name      = "Ubuntu"
   security_groups = ["mon site"]          # Groupe de sécurité avec règles appropriées
 
   tags = {
@@ -27,12 +27,9 @@ resource "aws_instance" "dev_instance" {
     # Tirer et exécuter l'image Docker
     sudo docker pull julesbestdev176/factorial
     sudo docker run -d -p 8080:8080 --name factorial-app julesbestdev176/factorial
-
-    # Cloner le dépôt Git et exécuter docker-compose
-    cd /home/ubuntu
-    git clone https://github.com/Ayib201/devops_app
-    cd devops_app
-    sudo docker compose up --build -d
+    docker pull ayibgoat/devopsapp
+    sudo docker run -d -p 80:80 ayibgoat/devopsapp
+    sudo docker start $(sudo docker ps -a -q)
   EOF
 }
 
